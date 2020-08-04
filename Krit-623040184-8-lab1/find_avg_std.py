18b62c90599839dd02323784323647d0e2cb3556
Krit-NameWalker <69181601+Krit-NameWalker@users.noreply.github.com>
2020-08-04 11:08:30 +0700

    Update find_avg_std.py

--- Krit-623040184-8-lab1/find_avg_std.py
+++ Krit-623040184-8-lab1/find_avg_std.py
@@ -7,7 +7,6 @@ n2 = random.randint(1, 10)
 print("Random integer : ", "[", n1, ",", n2, "]")
 
 Avg = (n1 + n2) / 2
-Std = math.sqrt(((n1 - Avg)*(n1 - Avg)+(n2 - Avg)*(n2 - Avg))/ 2)
+Std = math.sqrt(((n1 - Avg)*(n1 - Avg)+(n2 - Avg)*(n2 - Avg))/2)
 print("The average of ", n1, "and", n2, "is", Avg)
-print("The stadard deviation of", n1, "and", n2, "is", Std)
-
+print("The standard deviation of", n1, "and", n2, "is", Std)
